5cfd2e1e7489049881ba525da25645e1cc5e7e42
Gowtham <gowtham.chilakaluri@cognizant.com>
2026-02-16 18:13:30 +0530

    Add files via upload

---
+++ CustomerMS/src/main/java/CustomerMS/Entity/Customer.java
@@ -0,0 +1,36 @@
+package CustomerMS.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Customer {
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long customerId;
+	private String name;
+    private String email;
+
+}
---
+++ CustomerMS/src/main/java/CustomerMS/ExceptionHandling/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package CustomerMS.ExceptionHandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	 public ResponseEntity<String> handleException(Exception ex){
+		 return ResponseEntity
+				 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+				 .body(ex.getMessage());
+				 
+	 }
+
+}
